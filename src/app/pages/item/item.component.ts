5b4e9c68fc7e79118e409048545ca9211dc2cf95
erik ramirez <isc.erikdaniel@gmail.com>
2019-05-07 17:54:07 -0500

    pwa listo 1

---
+++ src/app/pages/item/item.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from '../../services/productos.service';
+import { ProductoDescripcion } from '../../interfaces/producto-descripcion.interface';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  producto: ProductoDescripcion;
+  id: string;
+
+  constructor( private route: ActivatedRoute,
+               public productosService: ProductosService) { }
+
+  ngOnInit() {
+    this.route.params
+        .subscribe(parametros => {
+          // console.log(parametros['id']);
+          // tslint:disable-next-line:no-string-literal
+          this.productosService.getProducto(parametros['id'])
+              .subscribe( (producto: ProductoDescripcion) => {
+                // tslint:disable-next-line:no-string-literal
+                this.id = parametros['id'];
+                this.producto = producto;
+               // console.log(producto);
+              });
+        });
+  }
+
+}
