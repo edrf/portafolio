b3ebe81ab9b366e368c3dda0023f973d5a13019e
erik ramirez <isc.erikdaniel@gmail.com>
2019-05-08 14:30:52 -0500

    modificacion app module /ngsw-worker.js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import { environment } from '../environments/environment';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ServiceWorkerModule.register('./ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
